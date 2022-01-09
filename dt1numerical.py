1402d1038c78a6b7e51a308cb8e8c7d01ff526d4
paidi12915 <96561180+paidi12915@users.noreply.github.com>
2022-01-09 18:57:35 +0530

    Add files via upload

---
+++ dt1numerical.py
@@ -0,0 +1,18 @@
+#data types
+print('data types')
+#Numearics(int,float, complex)
+marks=403                                 #402 is integer.
+percentage=(marks/600)*100               #percentage is floting point number.
+print('percentage', percentage)
+print(type(marks))
+print(type(percentage))
+
+#x+yi
+x=10+5j                                 #x is a complex no. with real and immaginary part.
+
+print(type(x))
+print(x.real)
+print(x.imag)
+print(x.conjugate())                      #x+yj conjugate is x-yj
+y=11+2j
+print(x+y)
